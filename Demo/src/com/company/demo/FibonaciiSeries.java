010f80ca0f9046985850699030d0ab6b57e1a5f7
Basavanagoud A <bsgoud78900@gmail.com>
2023-04-15 23:29:07 -0700

    added commints on fabonic series class.

--- Demo/src/com/company/demo/FibonaciiSeries.java
+++ Demo/src/com/company/demo/FibonaciiSeries.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 
 public class FibonaciiSeries {
     public static void main(String[] args) {
+        // Take the input from console for numbers of fibonacii series we want like 10 than returns until 10 num
         Scanner in=new Scanner(System.in);
+        // example 0,1,1,2,3,5,8,13,21,34
         int num = in.nextInt();
         int a,b=0;
         int c=1;
